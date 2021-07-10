4dd254ce7738ca202941194714273769c607b728
JorgeAlmeidaDeveloper <jorge.rummenigg@hotmail.com>
2021-07-10 20:16:30 -0300

    teste

---
+++ santader-maven/src/main/java/com/example/bootcamp/santadermaven/SantaderMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootcamp.santadermaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantaderMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantaderMavenApplication.class, args);
+	}
+
+}
---
+++ santader-maven/src/test/java/com/example/bootcamp/santadermaven/SantaderMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootcamp.santadermaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantaderMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
